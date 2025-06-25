32ad450aed939ffc01be49fccaca99060d1fa235
Yu Chun,Chou <a830144@gmail.com>
2025-06-25 15:27:58 -0500

    init build

--- src/main/java/com/mysap/sd/quotation/repository/QuotationItemRepository.java
+++ src/main/java/com/mysap/sd/quotation/repository/QuotationItemRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface QuotationItemRepository extends JpaRepository<QuotationItem, Long> {
-    List<QuotationItem> findByQuotationId(Long quotationId);
+
 }
